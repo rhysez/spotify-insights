5cb494ea78cfa576f0799fdb8e41da228d0264a0
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-16 16:10:27 +0100

    create profile app route with page

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,8 @@
 import {
-  UserGroupIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
   UserIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,14 +21,14 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 text-slate-200 bg-slate-800 p-3 text-sm font-medium hover:bg-slate-600 hover:text-green-500 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
