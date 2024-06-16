c8243e0bf56de1318df497bb2aafc98ed53ca63f
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-16 15:19:10 +0100

    install and configure shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,19 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    <main className="flex min-h-screen flex-col">
+      <div className="flex h-20 shrink-0 items-end bg-slate-800 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+      <div className="flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Spotify Insights</strong> <br />
+            Delivering a detailed view of your spotify profile.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-slate-800 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-green-500 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
