755c2bc5c37d396852ec2b01830e71fb484ba3b5
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-17 00:01:14 +0100

    add display string if categories are not available

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -11,7 +11,10 @@ export default async function Page() {
       <h2 className="font-bold text-4xl text-center md:text-left">Categories</h2>
       <main className="mt-10 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-2">
           {
+            categories ?
             categories.map((category: any, index: number) => <CategoryCard key={index} category={category} />)
+            :
+            <p>Could not fetch categories</p>
           }
       </main>
     </>
