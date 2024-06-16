c535a5c094a8e268f5003489dbec2a9e1ec2d482
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-16 22:52:40 +0100

    write getSpotifyToken async function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
+import { getSpotifyToken } from "../lib/data";
 
+export default async function Page() {
+  const token = await getSpotifyToken();
 
-export default function Page() {
+  console.log(token);
+
   return (
     <>
       <h2 className="font-bold text-4xl text-center md:text-left">Dashboard</h2>
-      
     </>
   );
 } 
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { User } from './definitions';
 
+
 export async function getUser(email: string) {
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
@@ -8,5 +9,24 @@ export async function getUser(email: string) {
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
+  }
+}
+
+export async function getSpotifyToken() {
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(process.env.SPOTIFY_CLIENT_ID as string + ':' + process.env.SPOTIFY_CLIENT_SECRET as string)
+      },
+      body: 'grant_type=client_credentials'
+    });
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error('Failed to fetch access token:', error);
+    throw new Error('Failed to fetch access token.');
   }
 }
