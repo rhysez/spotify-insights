daaa9f69b2352294ea40d28f421a3eb44c6b5455
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-23 17:25:18 +0100

    change hover styles

--- app/ui/dashboard/search/SearchItemCard.tsx
+++ app/ui/dashboard/search/SearchItemCard.tsx
@@ -15,14 +15,14 @@ import { Artist } from '@/app/lib/definitions';
 export default function SearchItemCard({ searchItem }: any) {
 
   return (
-      <Card className={clsx("bg-spotify_black border-none transition-opacity duration-150 ease-in-out")}>
+      <Card className={clsx("bg-spotify_black border-none transition-all duration-150 ease-in-out hover:bg-spotify_green cursor-pointer")}>
         <CardHeader>
           <CardTitle className="text-slate-200 text-center">{searchItem.name}</CardTitle>
         </CardHeader>
         <CardContent>
           {
             searchItem.images[0] ?
-            <Image className="mx-auto p-[2px] hover:bg-spotify_green cursor-pointer ease-in-out transition-all duration-150" width={250} height={300} src={searchItem.images[0].url} alt="artist image" />
+            <Image className="mx-auto p-[2px]" width={250} height={300} src={searchItem.images[0].url} alt="artist image" />
             :
             null
           }
