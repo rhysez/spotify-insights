bcc74b09d2d467eaeba79ebc2acbec61832e916a
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-24 20:00:40 +0100

    add dynamic genre tooltips to SearchItemCard component

--- app/dashboard/artist/loading.tsx
+++ app/dashboard/artist/loading.tsx
@@ -4,13 +4,13 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 space-y-12">
-      <div className="mx-auto flex flex-col justify-center space-y-3 mt-12">
+    <div className="flex flex-col">
+      <div className="mx-auto flex flex-col justify-center space-y-12 mt-12">
         <div className="space-y-2 text-center">
-          <Skeleton className="h-4 w-[200px] bg-spotify_dark_gray" />
+          <Skeleton className="h-52 w-64 rounded-lg mx-auto bg-spotify_dark_gray" />
         </div>
-        <Skeleton className="h-[400px] w-[80vw] rounded-xl bg-spotify_dark_gray" />
-        <Skeleton className="h-[400px] w-[80vw] rounded-xl bg-spotify_dark_gray" />
+        <Skeleton className="h-[50px] w-[150px] mx-auto rounded-xl bg-spotify_dark_gray" />
+        <Skeleton className="h-[400px] w-[500px] rounded-xl bg-spotify_dark_gray" />
       </div>
     </div>
   )
--- app/ui/dashboard/search/SearchItemCard.tsx
+++ app/ui/dashboard/search/SearchItemCard.tsx
@@ -6,6 +6,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import Image from 'next/image';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
@@ -15,32 +21,53 @@ export default function SearchItemCard({ searchItem }: any) {
   const pathname = usePathname();
 
   return (
-    <Link href={{
-      pathname: '/dashboard/artist',
-      query: { artist: searchItem.id }
-      }}>
-      <Card
-        className={clsx(
-          'block transform cursor-pointer border-none bg-spotify_dark_gray h-full transition-all duration-150 ease-in-out hover:bg-spotify_green',
-        )}
-      >
-        <CardHeader>
-          <CardTitle className="text-center text-slate-200">
-            {searchItem.name}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          {searchItem.images[0] ? (
-            <Image
-              className="mx-auto p-[2px]"
-              width={250}
-              height={300}
-              src={searchItem.images[0].url}
-              alt="artist image"
-            />
-          ) : null}
-        </CardContent>
-      </Card>
-    </Link>
+    <TooltipProvider>
+      <Tooltip delayDuration={1}>
+        <TooltipTrigger>
+          <Link
+            href={{
+              pathname: '/dashboard/artist',
+              query: { artist: searchItem.id },
+            }}
+          >
+            <Card
+              className={clsx(
+                'block h-full transform cursor-pointer border-none bg-spotify_dark_gray transition-all duration-150 ease-in-out hover:bg-spotify_green',
+              )}
+            >
+              <CardHeader>
+                <CardTitle className="text-center text-slate-200">
+                  {searchItem.name}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                {searchItem.images[0] ? (
+                  <Image
+                    className="mx-auto p-[2px]"
+                    width={250}
+                    height={300}
+                    src={searchItem.images[0].url}
+                    alt="artist image"
+                  />
+                ) : null}
+              </CardContent>
+            </Card>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent className="border-0 bg-transparent">
+          {searchItem.genres.length ? (
+            searchItem.genres.map((genre: any, index: number) => (
+              <p className="m-2 rounded-full bg-gradient-to-r from-spotify_white to-spotify_green p-2 text-center font-semibold shadow-lg">
+                {genre}
+              </p>
+            ))
+          ) : (
+            <p className="m-2 rounded-full bg-red-500 p-2 text-center font-semibold text-white shadow-lg">
+              No genres listed
+            </p>
+          )}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
