1e3d7b988fa12e9d881e57dd63cfb5307962ed8c
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-24 19:14:15 +0100

    add animated guitar icon to /dashboard view

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import SearchContainer from "../ui/dashboard/search/SearchContainer";
 import { Suspense } from "react";
 import Loading from "./loading";
+import Icon from '@mdi/react';
+import { mdiGuitarElectric } from '@mdi/js';
 
 export default async function Page() {
 
   return (
     <section className="flex-col space-y-10">
       <div className="space-y-3">
+        <Icon className="mx-auto mb-10 text-spotify_green animate-[bounce_2s_linear_infinite]" path={mdiGuitarElectric} size={4} />
         <h2 className="text-center md:text-5xl text-4xl font-bold md:mt-36">Discover insights</h2>
         <p className="text-center md:text-xl text-xl font-bold text-spotify_green">Search for an artist...</p>
       </div>
