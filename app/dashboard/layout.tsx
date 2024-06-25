5dfd4a6e5e75b255f896f24f76cf0f2ea192d6d3
Rhys <rhyshodgson97@gmail.com>
2024-06-25 18:38:49 +0100

    fix merge conflict

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,4 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import LoadingCategories from './categories/LoadingCategories';
-import { Suspense } from 'react';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
