c55d4254987057c6c2833be1e0b42ac5809dd921
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-29 14:12:56 +0100

    set default route for login

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import Icon from '@mdi/react';
+import { mdiSpotify } from '@mdi/js';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import LoginForm from '@/app/ui/login-form';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <header className="h-30 flex flex-col md:flex-row shrink-0 items-center justify-center gap-2 bg-spotify_black p-4 md:h-48">
+        <Icon className="text-green-500" path={mdiSpotify} size={5} />
+        <h2 className="font-bold text-spotify_green md:text-5xl text-3xl text-center">Spotify Insights</h2>
+      </header>
+      <LoginForm />
+    </main>
+  );
+}
