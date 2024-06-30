02ce25bde2f7ea1e126226be253f3af027957b65
Rhys Hodgson <rhyshodgson97@gmail.com>
2024-06-30 10:06:28 +0100

    add /signup route, create signup-form.tsx

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -4,6 +4,7 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
@@ -11,7 +12,7 @@ import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import Link from 'next/link';
 
-export default function LoginForm() {
+export default function SignupForm() {
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
@@ -22,8 +23,27 @@ export default function LoginForm() {
       <div className="flex justify-center rounded-lg ">
         <div className="mx-auto w-[95%] rounded-md border-[1px] border-spotify_link_active bg-spotify_dark_gray p-4 md:w-[70%] lg:w-[40%] xl:w-[30%]">
           <h1 className={`${inter.className} mb-3 text-center text-2xl`}>
-            Log in to your account
+            Account creation
           </h1>
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs  text-spotify_white"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm text-spotify_black outline-2 placeholder:text-gray-500"
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Pick a username"
+                required
+              />
+              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-spotify_black peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs  text-spotify_white"
@@ -64,8 +84,8 @@ export default function LoginForm() {
             </div>
           </div>
           <div className="mt-12">
-            <LoginButton aria-disabled={isPending} />
             <SignupButton />
+            <LoginButton aria-disabled={isPending} />
           </div>
         </div>
         <div className="flex h-8 items-end space-x-1">
@@ -83,19 +103,20 @@ export default function LoginForm() {
 
 function LoginButton() {
   return (
-    <Button className="mt-4 w-full bg-spotify_green text-spotify_white transition-all ease-in-out hover:bg-spotify_white hover:text-spotify_green">
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    <Link href="/login">
+      <Button className="mt-4 w-full bg-spotify_green text-spotify_white transition-all ease-in-out hover:bg-spotify_white hover:text-spotify_green">
+        Existing user? Log in{' '}
+        <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      </Button>
+    </Link>
   );
 }
 
 function SignupButton() {
   return (
-    <Link href='/signup'>
-      <Button className="mt-4 w-full border-2 border-spotify_green bg-spotify_white text-spotify_link_active transition-all ease-in-out hover:bg-spotify_white hover:text-spotify_green">
-        Sign up{' '}
-        <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-      </Button>
-    </Link>
+    <Button className="mt-4 w-full border-2 border-spotify_green bg-spotify_white text-spotify_link_active transition-all ease-in-out hover:bg-spotify_white hover:text-spotify_green">
+      Create my account{' '}
+      <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
   );
 }
